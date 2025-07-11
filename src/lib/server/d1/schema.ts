7613b65624a574bfb438e8c6a07dcf22f7d3a1ba
장효찬 <jang@hyochan.site>
2025-07-11 17:50:12 +0900

    feat: .gitignore에 drizzle 추가 및 createdAt 필드의 기본값을 SQL로 수정

--- src/lib/server/d1/schema.ts
+++ src/lib/server/d1/schema.ts
@@ -1,16 +1,17 @@
 import { create } from 'domain';
+import { sql } from 'drizzle-orm';
 import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core';
 
 export const creator = sqliteTable("creator", {
 	uuid: text().primaryKey(),
 	creator: text().notNull(),
 	password: text().notNull(),
-	createdAt: integer().notNull().default(Date.now()),
+	createdAt: integer().notNull().default(sql`(strftime('%s', 'now'))`),
 });
 
 export const board = sqliteTable("board", {
 	uuid: text().primaryKey(),
 	creator: text().notNull().references(() => creator.uuid),
 	name: text().notNull(),
-	createdAt: integer().notNull().default(Date.now()),
+	createdAt: integer().notNull().default(sql`(strftime('%s', 'now'))`),
 });
