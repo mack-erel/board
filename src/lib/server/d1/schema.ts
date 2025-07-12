664ba6881c1fdbbfcf80c35565c78c679855381b
Henry Jang <jang@hyochan.site>
2025-07-12 23:28:38 +0900

    feat: 데이터베이스 스키마 수정 및 게시글 테이블 추가
    - createdAt 필드의 타입을 integer에서 real로 변경
    - creator 및 board 테이블의 createdAt 필드 기본값 수정
    - article 테이블 추가: uuid, board, subject, content, additional, createdAt 필드 포함

--- src/lib/server/d1/schema.ts
+++ src/lib/server/d1/schema.ts
@@ -1,17 +1,26 @@
 import { create } from 'domain';
 import { sql } from 'drizzle-orm';
-import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core';
+import { sqliteTable, integer, text, real } from 'drizzle-orm/sqlite-core';
 
 export const creator = sqliteTable("creator", {
 	uuid: text().primaryKey(),
 	creator: text().notNull(),
 	password: text().notNull(),
-	createdAt: integer().notNull().default(sql`(strftime('%s', 'now'))`),
+	createdAt: real().notNull().default(sql`(round((strftime('%s') - strftime('%S') + strftime('%f')) * 10000))`),
 });
 
 export const board = sqliteTable("board", {
 	uuid: text().primaryKey(),
 	creator: text().notNull().references(() => creator.uuid),
 	name: text().notNull(),
-	createdAt: integer().notNull().default(sql`(strftime('%s', 'now'))`),
+	createdAt: real().notNull().default(sql`(round((strftime('%s') - strftime('%S') + strftime('%f')) * 10000))`),
+});
+
+export const article = sqliteTable("article", {
+	uuid: text().primaryKey(),
+	board: text().notNull().references(() => board.uuid),
+	subject: text().notNull(),
+	content: text().notNull(),
+	additional: text().notNull().default("{}"),
+	createdAt: real().notNull().default(sql`(round((strftime('%s') - strftime('%S') + strftime('%f')) * 10000))`),
 });
